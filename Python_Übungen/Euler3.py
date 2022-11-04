10d853781ec2ee4b59a693536771182fad595cdb
Tim Porger <tim.porger@mitutoyo-ctl.de>
2022-11-04 16:03:48 +0100

    Refactoring

--- Python_Übungen/Euler3.py
+++ Python_Übungen/Euler3.py
@@ -3,25 +3,26 @@
 # What is the largest prime factor of the number 600851475143
 from math import sqrt
 
-def find_all_prime_factor(n :int):
 
-    primefactor = 3
+def find_prime_factor(n: int):
     primefactors_of_n = []
-    while True:
-        if check_num_is_prime(primefactor):
-            if (n % primefactor) == 0:
-                primefactors_of_n.append(primefactor)
-                n = n // primefactor
-                if check_num_is_prime(n):
-                    primefactors_of_n.append(n)
-                    return primefactors_of_n
-            else:
-                primefactor = primefactor + 2
-        else:
-            primefactor = primefactor + 2
+    n = append_primefactors(n, 2, primefactors_of_n)
+    prime_factor = 3
+    while not check_num_is_prime(n):
+        n = append_primefactors(n, prime_factor, primefactors_of_n)
+        prime_factor = prime_factor + 2
+    primefactors_of_n.append(n)
+    return primefactors_of_n
 
 
-def check_num_is_prime(n :int):
+def append_primefactors(n, prime_factor, primefactors_of_n):
+    while (n % prime_factor) == 0:
+        primefactors_of_n.append(prime_factor)
+        n = n // prime_factor
+    return n
+
+
+def check_num_is_prime(n: int):
     if n > 1:
         for i in range(2, int(sqrt(n))):
             if (n % i) == 0:
@@ -29,13 +30,4 @@ def check_num_is_prime(n :int):
         return True
 
 
-
 print(max(find_prime_factor(600851475143)))
-
-
-
-
-
-
-
-
