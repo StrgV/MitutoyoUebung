30900dfd8dee4cc5a25b7e40c50e62c706586dd7
Tim Porger <tim.porger@mitutoyo-ctl.de>
2022-11-18 15:07:45 +0100

    Main method check

--- Python_Übungen/Euler4.py
+++ Python_Übungen/Euler4.py
@@ -4,12 +4,12 @@
 
 def calculate_and_check(index1: int, index2: int) -> int:
     check_for = index1 * index2
-    if check_palindrome(check_for):
+    if is_palindrome(check_for):
         return check_for
     return 0
 
 
-def check_palindrome(check_for):
+def is_palindrome(check_for):
     length = len(str(check_for))
     for i in range(1, round(length / 2) + 1):
         if int(str(check_for)[i - 1]) != int(str(check_for)[-i]):
@@ -36,7 +36,9 @@ def find_largest_palindrome(digits: int):
 
 assert calculate_and_check(91, 99) == 9009
 assert calculate_and_check(12, 12) == 0
-assert check_palindrome(3663)
+assert is_palindrome(3663)
 assert find_all_palindromes_for_n_digits(1) == [9, 8, 7, 6, 5, 8, 4, 9, 6, 3, 8, 6, 4, 2, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 assert find_largest_palindrome(1) == 9
-print(find_largest_palindrome(3))
+
+if __name__ == "__main__":
+    print(find_largest_palindrome(3))
